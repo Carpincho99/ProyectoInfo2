882a19a6e1b074dedf0703b0046b7c89cd364496
Carpincho99 <ezegiorgis@hotmail.com>
2022-09-10 21:17:09 -0300

    init GUI QT

--- software/micro/inc/stepper.h
+++ software/micro/inc/stepper.h
@@ -1,6 +1,9 @@
+#ifndef __STDINT_H_
+#include <stdint.h>
+#endif
+
 #ifndef stepper
 #define stepper
-#include "gcode.h"
 
 void execLine(uint8_t, int16_t);
 void moveAxisRelative(const uint8_t*, int16_t);
