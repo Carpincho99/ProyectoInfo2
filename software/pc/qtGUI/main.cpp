5a3fa38a9f40db4b017f08ccfe4229209141a762
Carpincho99 <ezegiorgis@hotmail.com>
2022-09-13 12:29:41 -0300

    Revert "working serial GUI"
    This reverts commit 5573c796398dbd50f55631a76df49f829b775b7b.

--- software/pc/qtGUI/main.cpp
+++ software/pc/qtGUI/main.cpp
@@ -1,62 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QSerialPort>
-#include <QSerialPortInfo>
-#include <QDebug>
-
-
-QSerialPort arduino;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    QString arduino_uno_port_name;
-
-
-    static const quint16 arduino_uno_vendor_id = 9025;
-    static const quint16 arduino_uno_product_id = 1;
-
-    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
-        if(serialPortInfo.hasProductIdentifier() && serialPortInfo.hasVendorIdentifier()){
-            if((serialPortInfo.productIdentifier() == arduino_uno_product_id) && (serialPortInfo.vendorIdentifier() == arduino_uno_vendor_id)){
-                arduino_uno_port_name = serialPortInfo.portName();
-            }
-        }
-    }
-
-    arduino.setPortName(arduino_uno_port_name);
-    if(!arduino.setBaudRate(QSerialPort::Baud115200))
-        qDebug() << arduino.errorString();
-    if(!arduino.setDataBits(QSerialPort::Data8))
-        qDebug() << arduino.errorString();
-    if(!arduino.setParity(QSerialPort::NoParity))
-        qDebug() << arduino.errorString();
-    if(!arduino.setStopBits(QSerialPort::OneStop))
-        qDebug() << arduino.errorString();
-    if(!arduino.setFlowControl(QSerialPort::NoFlowControl))
-        qDebug() << arduino.errorString();
-    if(!arduino.open(QIODevice::ReadWrite))
-        qDebug() << arduino.errorString();
-
-    if(!arduino.waitForReadyRead(-1)) //block until new data arrives
-        qDebug() << "error: " << arduino.errorString();
-    else{
-        QByteArray data = arduino.readAll();
-        qDebug() << data;
-    }
-
-
     return a.exec();
-}
-
-void MainWindow::on_xPositiveButton_clicked()
-{
-    if(arduino.isWritable()){
-        arduino.write("x-10\n");
-    }
-
 }
